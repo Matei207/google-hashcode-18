d272ead52431b6ace9b1b81cd001c12bf9602c3a
Matei David <matei.david.35@gmail.com>
2018-03-01 18:05:58 +0000

    Added basic DS layout (as good as blueprints can get in js)

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello, World!");
+let input = {
+    grid: {
+        rowNumber: 0,
+        colNumber: 0,
+    },
+    numberVehicles:0,
+    numberRides: 0,
+    rideBonus:0,
+    steps:0,
+    individualRides:[],
+};
+
+input.individualRides.push("HELLO");
+console.log(input.individualRides[0]);
