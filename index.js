c3b7405ef1bfc7620009f9e8a2ee6bb21c411ab9
Alex Mackenzie <alex@alexmack.net>
2018-03-01 18:13:22 +0000

    Read input header line

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-console.log("Hello, World!");
 let input = {
     grid: {
-        rowNumber: 0,
-        colNumber: 0,
+        rows: 0,
+        cols: 0,
     },
-    numberVehicles:0,
-    numberRides: 0,
-    rideBonus:0,
-    steps:0,
+    vehicles:0,
+    rides: 0,
+    bonus: 0,
+    steps: 0,
     individualRides:[],
 };
 
-input.individualRides.push("HELLO");
-console.log(input.individualRides[0]);
+let filepath = "./datasets/a_example.in";
+
+const fs = require("fs");
+
+let lines = fs.readFileSync(filepath, {
+	encoding: "UTF-8"
+});
+lines = lines.split("\n");
+
+let header = lines[0].split(" ");
+input.grid.rows = parseInt(header[0]);
+input.grid.cols = parseInt(header[1]);
+input.vehicles = parseInt(header[2]);
+input.rides = parseInt(header[3]);
+input.bonus = parseInt(header[3]);
+input.steps = parseInt(header[4]);
+
+console.log(input);
